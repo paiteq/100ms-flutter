db5234f3eff3a1a6fd4056a8548f1c4f69566d4c
Shivani <shivani.sharma@gmail.com>
2024-09-11 18:10:26 +0530

    reverted onRoomEndCrossTap call back

--- packages/hms_room_kit/lib/src/widgets/common_widgets/hms_left_room_screen.dart
+++ packages/hms_room_kit/lib/src/widgets/common_widgets/hms_left_room_screen.dart
@@ -35,13 +35,11 @@ class HMSLeftRoomScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   InkWell(
-                    onTap: () =>
-                        Constant.onRoomEndCrossTap ?? Navigator.pop(context),
-                    // onTap: () => {
-                    //   ///Here we reset the layout colors and pop the leave screen
-                    //   HMSThemeColors.resetLayoutColors(),
-                    //   Navigator.pop(context),
-                    // },
+                    onTap: () => {
+                      ///Here we reset the layout colors and pop the leave screen
+                      HMSThemeColors.resetLayoutColors(),
+                      Navigator.pop(context),
+                    },
                     child: CircleAvatar(
                       radius: 24,
                       backgroundColor: HMSThemeColors.surfaceDefault,
